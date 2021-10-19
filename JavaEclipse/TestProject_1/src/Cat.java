530179199eea02117955d880b580499bd89db674
jldroid25 <jldroid25@protonmail.com>
2021-10-19 18:13:25 -0400

    updted cat class

--- JavaEclipse/TestProject_1/src/Cat.java
+++ JavaEclipse/TestProject_1/src/Cat.java
@@ -15,20 +15,18 @@ public class Cat  extends Animal {
 	}
 	
 	//Class Animal Sound Method
-		public void biCatSound(String tigerSound) {
-			System.out.println( " I AM STARVING NEED TO EAT HUMAN ASAP !") ;
-		}
+	public void biCatSound(String tigerSound) {
+		System.out.println( " I AM STARVING NEED TO EAT HUMAN ASAP !") ;
+	}
+	//Overriden Method  Same name as Class Animal one diff body
+	public void makeNoise(String animalSound) {
+		System.out.println( " animalSound From CAT Class") ;
+	}
 
-		//Class Animal Sound Method
-		public void makeNoise(String animalSound) {
-			System.out.println( " animalSound From CAT Class") ;
-		}
-		
 	//main
 	public static void main(String[] args) {
-		
+
 		Animal animal1 = new Cat(1000, 2, 1, 4, 1, 1);
-	
 		//System.out.println(animal1.fur); // can't access fur from CAt class
 		//System.out.println(animal1.eyes); // can't access  eyes from Cat class.
 
@@ -36,16 +34,12 @@ public class Cat  extends Animal {
 		System.out.println(animal1.feet); // can access feet from Animal
 		
 		System.out.println("Number of Head  Cat  :");
-		System.out.println(animal1.head); // can't access fur
+		System.out.println(animal1.head); // can't access head
 		
 		//Overriden method but will print from Cat Class
 		animal1.makeNoise("MEOW "); //can access methods on Animal 
 		
-		//animal1.bigCatSound(); // Cannot access 
+		//animal1.bigCatSound(); // Cannot access method define on class CAT.
 
-
-
-		
 	}//main
-
 }//class
